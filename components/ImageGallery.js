2779c6eebe359c3206294627cd06dde4925017e3
Tobias Lüger <tobias-lueger@web.de>
2024-09-19 17:36:10 +0200

    added gallery

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -3,6 +3,7 @@ import { Masonry } from 'react-masonry'
 import Modal from "./Modal";
 
 import { useState } from "react"
+import Image from "next/legacy/image";
 
 const ImageGallery = ({ blok }) => {
 
@@ -12,21 +13,31 @@ const ImageGallery = ({ blok }) => {
     setClickedImg(item.src.filename);
   };
 
+  console.log(blok)
+
   return (
     <>
       <Masonry>
-        {blok.images.map((image, index) => (
-          <div className="w-full md:w-1/2 lg:w-1/3" key={index}>
-            <img {...storyblokEditable(blok.images[index])} className="h-auto w-auto cursor-pointer transition-all hover:scale-95" src={image.src.filename} alt="test" onClick={() => handleClick(image, index)} />
+        {blok.images.map((image, index) => {
+          
+          let dimensions = {
+            width: image.src.filename.split('/')[5].split('x')[0],
+            height: image.src.filename.split('/')[5].split('x')[1]
+          }
+          console.log("dimensions: ", dimensions.width, dimensions.height)
+          return (
+          <div className="w-full h-fit md:w-1/2 lg:w-1/3" key={index}>
+            <Image {...storyblokEditable(blok.images[index])} className="cursor-pointer transition-all hover:scale-95" src={image.src.filename} alt="test" width={dimensions.width} height={dimensions.height} priority={true} objectFit="contain" onClick={() => handleClick(image, index)} />
           </div>
-        ))}
+          );
+        })}
       </Masonry>
       {clickedImg && (
-          <Modal
-            clickedImg={clickedImg}
-            setClickedImg={setClickedImg}
-          />
-        )}
+        <Modal
+          clickedImg={clickedImg}
+          setClickedImg={setClickedImg}
+        />
+      )}
     </>
 
   )
